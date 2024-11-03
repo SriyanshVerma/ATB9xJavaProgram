8eb20cd3cacfa4fcd1113a76d920fb38e40e1ffd
Sriyansh Verma <sriyanshverma@Sriyanshs-MacBook-Air.local>
2024-11-03 19:23:56 +0530

    04102024

---
+++ src/ex_03102024/Lab002.java
@@ -0,0 +1,14 @@
+package ex_03102024;
+/*
+*this is multiple line comments
+* this is used for documentation purpose
+* Author- Pramod
+*
+*
+*
+*
+* */
+public class Lab002 {
+    public static void main(String[] args) {
+}
+}
---
+++ src/ex_15102024/lab060.java
@@ -0,0 +1,9 @@
+package ex_15102024;
+
+public class lab060 {
+    public static void main(String[] args) {
+        int age= 19;
+                String result = age > 18 ? "Allowed to Vote" : "Not allowed to Vote";
+        System.out.println(result);
+    }
+}
